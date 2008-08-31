48ad43e50ad0bff5e41e2abf10ee7603f5d16646
me@jeffscudder.com <me@jeffscudder.com@dbae202d-5355-0410-96b6-d56ebb4989d0>
2008-08-31 00:48:16 +0000

    Removing unused import.

--- lite/main.py
+++ lite/main.py
@@ -16,7 +16,6 @@
 import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 from google.appengine.api import memcache
 
