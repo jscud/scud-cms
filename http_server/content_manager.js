9856195682918453e83d4031393b7a9dcb1e9a7a
jscud <me@jeffscudder.com>
2015-07-03 11:44:50 -0700

    Add an httpRequest function to content manger client JavaScript.

--- http_server/content_manager.js
+++ http_server/content_manager.js
@@ -1,3 +1,25 @@
-function test() {
 
+function httpRequest(httpVerb, data, url, headers, handler) {
+  var http = null;
+  if (window.XMLHttpRequest) {
+    http = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    http = new ActiveXObject('Microsoft.XMLHTTP');
+  }
+  if (http) {
+    http.open(httpVerb, url, true);
+    http.onreadystatechange = function() {
+      if (http.readyState == 4) {
+        handler(http);
+      }
+    };
+    var propery = null;
+    for (property in headers) {
+      http.setRequestHeader(property, headers[property]);
+    }
+    http.send(data);
+  } else {
+    throw new Error('Unable to create the HTTP request object.');
+  }
 }
+
