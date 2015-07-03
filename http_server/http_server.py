1cd5fd68c73d3f3d1e32a176dcd0f7bcd677e053
jscud <me@jeffscudder.com>
2015-07-02 23:32:00 -0700

    Beginnings of a Resource entity model

--- http_server/http_server.py
+++ http_server/http_server.py
@@ -1,4 +1,17 @@
+from google.appengine.ext import ndb
 import webapp2
+
+class Resource(ndb.Model):
+    """Contents of a single URL."""
+    path = ndb.StringProperty()
+    content = ndb.TextProperty()
+    content_type = ndb.StringProperty()
+
+
+class ResourceRenderer(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, HTTP')
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
